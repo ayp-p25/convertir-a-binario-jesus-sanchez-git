11ab2afe8b1eb14e496592ea0c3d4fe92e12b873
jesusa-sanchez-git <jesusa.sanchez@iteso.mx_PianoMan11VJ>
2025-03-22 19:24:10 -0600

    First

--- convertir_a_binario.py
+++ convertir_a_binario.py
@@ -1,15 +1,13 @@
-"""
-Inserta el encabezado aquí y escribe tu código abajo
-"""
-
-# Declaraciones
-CONSTANTE = valor
-
-# Entradas
-entrada = input()
-
-# Proceso
-
-
-# Salidas
-print(salida)
+N = int(input("Ingrese el numero del que quiera saber su binario : \n"))
+def binario(N):
+    R = 0
+    B = ""
+    if N == 0 :
+        return 0
+    while N>=1:
+        R = int(N%2)
+        N = int(N/2)
+        B = str(R)+B
+    return B
+NB = binario(N)
+print(f"El numero {N} en binario es {NB}")
